c9662237db1248c0fa697cac6178283626038c45
thales <thalesc@utfpr.edu.br>
2017-07-19 22:59:11 -0300

    something

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main():
     
     # Getting the URL based on the sign and accessing it
     source = urllib.request.urlopen('https://www.astrology.com/horoscope/daily/' + sign + '.html')
-    soup = bs.BeautifulSoup(source, 'lxml')
+    site = bs.BeautifulSoup(source, 'lxml')
 
     # Finding the div with  the prediction
-    horoscope = soup.find('div' , {'class': 'page-horoscope-text'})
+    horoscope = site.find('div' , {'class': 'page-horoscope-text'})
 
     # Printing
-    print("\n", soup.title.text, "\n \n", horoscope.get_text(), "\n")
+    print("\n", site.title.text, "\n \n", horoscope.get_text(), "\n")
 
 if __name__ == "__main__" : main()
 
