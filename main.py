eb03e64078059fe91a6b1da1cbab6bd9999424f5
thales <thalessc@alunos.utfpr.edu.br>
2017-07-19 22:49:28 -0300

    little changes 2

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
-#Importing BeautifulSoup for scraping
+# Importing BeautifulSoup for scraping
 import bs4 as bs
 import urllib.request
 import sys
 
-def main():
-    #Getting the sign as parameter
+def main(): 
+    # Getting the sign as parameter
     sign = sys.argv[1]
+    
+    # Getting the URL based on the sign and accessing it
     source = urllib.request.urlopen('https://www.astrology.com/horoscope/daily/' + sign + '.html')
     soup = bs.BeautifulSoup(source, 'lxml')
-    #Finding the div with  the prediction
+
+    # Finding the div with  the prediction
     horoscope = soup.find('div' , {'class': 'page-horoscope-text'})
-    #Printing
-    print(soup.title.text, "\n", horoscope.get_text())
+    # Printing
+    print("\n", soup.title.text, "\n \n", horoscope.get_text(), "\n")
 
 if __name__ == "__main__" : main()
 
